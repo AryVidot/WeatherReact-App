2ecff4505c9a78c6e186046045b3ed3b9d07116f
Ary Vidot <aryvid@gmail.com>
2020-08-04 11:45:48 +0100

    add CSS in index.css

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import "./index.css";
+
+const api = {
+  key: "b3eeac9873199e3b6c23996e0eb4ab5e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            autoComplete="on"
+            required
+            placeholder="City..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
